e24d374e5fc81317bb4e47e78c6d439b5a7f6819
Rodrigo <r-alexander@hotmail.com>
2020-08-20 13:58:38 -0300

    pasta build

---
+++ build/precache-manifest.627b9002098fe896242de3233b739552.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ffa3ee703c7127d84bc3a97ccd36de",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3bbe0cdf294158554cc6",
+    "url": "./static/css/2.d53a13f9.chunk.css"
+  },
+  {
+    "revision": "d449d1d826aa8fe75666",
+    "url": "./static/css/main.d2a6f7e7.chunk.css"
+  },
+  {
+    "revision": "3bbe0cdf294158554cc6",
+    "url": "./static/js/2.f20cf934.chunk.js"
+  },
+  {
+    "revision": "e9b70ed94c92560626f6d789c6814c32",
+    "url": "./static/js/2.f20cf934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d449d1d826aa8fe75666",
+    "url": "./static/js/main.6ace4ac9.chunk.js"
+  },
+  {
+    "revision": "72c2c2056c77ec67c2e4",
+    "url": "./static/js/runtime-main.b6f12d70.js"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "./static/media/sidebar-2.310509c9.jpg"
+  }
+]);
